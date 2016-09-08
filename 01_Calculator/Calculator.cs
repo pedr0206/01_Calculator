c59677446b00ecedcd1dea6c5751a34a7bf82ea6
Ortega <Ortega@DESKTOP-2PFGEBL>
2016-09-08 12:06:58 +0200

    Add project files.

---
+++ 01_Calculator/Calculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _01_Calculator
+{
+    internal class Calculator
+    {
+        internal static int Add(int v1, int v2)
+        {
+
+            return v1 + v2;
+        }
+
+        internal static int Subtract(int v1, int v2)
+        {
+            return v1 - v2;
+        }
+
+        internal static int Multiply(int v1, int v2)
+        {
+            return v1 * v2;
+        }
+
+        internal static int Power(int v1, int v2)
+        {
+            return (int)Math.Pow(v1, v2);
+        }
+
+        internal static int Factorial(int v)
+        {
+            int result = 1;
+            int (int i = 1; int = v; int++);
+        }
+        
+    }
+}
